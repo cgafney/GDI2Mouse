b58a9c2a705e8200c4662dbc634e4cc73de894a2
cgafney <123777742+cgafney@users.noreply.github.com>
2023-02-16 20:54:59 -0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,20 @@
+function play1() {
+  let audio1 = document.getElementById("audio1");
+  audio1.currentTime = 0;
+  audio1.play();
+}
+function play2() {
+  let audio2 = document.getElementById("audio2");
+  audio2.currentTime = 0;
+  audio2.play();
+}
+function play3() {
+  let audio3 = document.getElementById("audio3");
+  audio3.currentTime = 0;
+  audio3.play();
+}
+function play4() {
+  let audio4 = document.getElementById("audio4");
+  audio4.currentTime = 0;
+  audio4.play();
+}
